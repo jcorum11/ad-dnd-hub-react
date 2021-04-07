2e18696e62299981d5ae9d2252450574acccc472
Jacob Corum <jacobcorum@Jacobs-MacBook-Pro.local>
2021-04-07 15:18:50 -0600

    basic set up of app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { BrowserRouter as router, route, Switch } from "react-router-dom";
-import Home from './pages/Home'
-import Logs from './pages/Logs'
-import Maps from './pages/Maps'
-import Players from './pages/Players'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Home from "./pages/Home";
+import Logs from "./pages/CapaignLogs";
+import Maps from "./pages/Maps";
+import Players from "./pages/Players/Players";
 
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <Router>
-    <Header></Header>
-  </Router>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/logs" component={Logs} />
+        <Route exact path="/maps" component={Maps} />
+        <Route exact path="/players" component={Players} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
